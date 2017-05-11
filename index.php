3ee9268aa28282f042b65ce0b57110eb05d69cb1
Amanda Bukur <amandabukur11@gmail.com>
2017-05-11 00:31:48 -0700

    added images and main nav

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <!-- Google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Permanent+Marker" rel="stylesheet">
+        <meta charset="utf-8">
+        <title>Lali's Kitchen</title>
+    </head>
+    <body>
+<?php include("header.html") ?>
+<div class="site-wrapper" id="site-wrapper" style="">
+
+</div>
+
+
+<?php include("footer.html") ?>
